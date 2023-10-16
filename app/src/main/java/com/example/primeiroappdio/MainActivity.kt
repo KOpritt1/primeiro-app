93553f5bf11c27b5d5d64804edf0a97367eb81ee
KOpritt1 <kopritt.mateus@gmail.com>
2023-10-16 18:50:03 -0300

    Commit desafio de internacionalização do Bootcamp

---
+++ app/src/main/java/com/example/primeiroappdio/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.primeiroappdio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //criar valor dentro do string.xml
+        //Trocar o texto do xml e tornar internacional(qualquer idioma)
+    }
+
+}
